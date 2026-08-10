881561f98f0c631a451dfbef7d220c7f708cb4b8
DaniloBiletskiy <danilobiletskiy@users.noreply.github.com>
2026-08-11 02:39:51 +0300

    Tighten scroll pacing: 1500vh/1400vh, scrub 0.75/0.5

--- src/animation.ts
+++ src/animation.ts
@@ -84,7 +84,7 @@ export function initStory(root: HTMLElement, onReady: (g: GotoFn) => void): () =
             trigger: root,
             start: 'top top',
             end: 'bottom bottom',
-            scrub: 1,
+            scrub: mobile ? 0.5 : 0.75,
             invalidateOnRefresh: true,
           },
           onUpdate: () => {
