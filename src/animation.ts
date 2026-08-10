c069c2909d95d9ea5788fc1959d84aa199f6bff5
DaniloBiletskiy <danilobiletskiy@users.noreply.github.com>
2026-08-11 02:17:07 +0300

    Slow animation progress 3x (2100vh story), scrub 1.0

--- src/animation.ts
+++ src/animation.ts
@@ -84,7 +84,7 @@ export function initStory(root: HTMLElement, onReady: (g: GotoFn) => void): () =
             trigger: root,
             start: 'top top',
             end: 'bottom bottom',
-            scrub: 1.6,
+            scrub: 1,
             invalidateOnRefresh: true,
           },
           onUpdate: () => {
