e4b69a1f63e7fbadad4506a670af2187ba85c728
DaniloBiletskiy <danilobiletskiy@users.noreply.github.com>
2026-08-11 03:10:31 +0300

    Mobile scroll: 1600vh story, scrub 0.9

--- src/animation.ts
+++ src/animation.ts
@@ -84,7 +84,7 @@ export function initStory(root: HTMLElement, onReady: (g: GotoFn) => void): () =
             trigger: root,
             start: 'top top',
             end: 'bottom bottom',
-            scrub: mobile ? 0.68 : 0.88,
+            scrub: mobile ? 0.9 : 0.88,
             invalidateOnRefresh: true,
           },
           onUpdate: () => {
